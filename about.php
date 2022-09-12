e5f2b680bde7758d1fe36f781bcbc111b89ed32a
JAWA Production <jcawa@denr.gov.ph>
2022-09-12 10:05:23 +0800

    Update about.php

--- about.php
+++ about.php
@@ -1,13 +1,4 @@
-<section id="aboutus">
-	<div class="container-fluid bg-dark bg-gradient">
-		<div class="container text-white">
-			<br><br>
-			<h1 class="mb-2"><strong>About OLDRMS</h1></strong>
-			<p>A certificate/document issued to a dealer of lumber,logs, poles or piles upon registration with the DENR. (Republic Act No. 1239, series of 1955 and FAO 26, series of 1956, DAO 1994-08).</p>
-			<br><br>
-		</div>
-	</div>
-
+<section id="indexmap">
 <div class="container">
 	<div class="bg-dark mt-5 mb-5 border border-dark border-5">
 		<h1 class="text-white text-center"><strong>LUMBER DEALER PERMITTEES</strong></h1>
@@ -23,4 +14,27 @@
 	</div>	
 </div>
 </section>
+
+<section id="aboutus" class="bg-dark bg-gradient">
+	<div class="container-fluid">
+		<div class="container text-white">
+			<br><br>
+			<h1 class="mb-2"><strong>About OLDRMS</h1></strong><br>
+			<p>The Online Lumber Dealer Registration and Monitoring System is developed in order to:</p>
+			<li>Standardize and streamline the process flow regionwide</li>
+			<li>Strengthen the  protection and conservation of naturally grown timber</li>
+			<li>Improve access to service delivery and efficient processing of lumber dealer application</li>
+			<li>Update statistics of all log/lumber supply contracts and aid the analysis of wood volume available versus the wood requirement</li>
+			<li>Minimize face-to-face transactions between and among the clients and DENR personnel</li>
+			<br><br><br>
+
+			<h1 class="mb-2"><strong>MANDATE</h1></strong><br>
+			<P>“The DENR shall be the primary government agency responsible for the conservation, management, development, and proper use of the country’s environment and natural resources, specifically forest and grazing lands, mineral resources, including those in reservation and watershed areas, and lands of the public domain, as well as the licensing and regulation of all natural resources as may be provided for by law in order to ensure equitable sharing of the benefits derived therefrom for the welfare of the present and future generations of Filipinos.”</p>
+			<br><br><br><br><br>
+		</div>
+	</div>
+</section>
+
+
+
 
