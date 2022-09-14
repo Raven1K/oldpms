4e5a6ea28b51b6d4bfbb6a3466d9ed8aad7a0a80
s0greedy <jaycee234@gmail.com>
2022-09-14 10:19:45 +0800

    dashboard FUS main

---
+++ main/footerdash.php
@@ -0,0 +1,12 @@
+<!-- Footer -->
+            <footer class="sticky-footer bg-dark border-top-bottom shadow h-40 py-2">
+                <div class="container my-auto">
+                    <div class="copyright text-white my-auto border-top-0">
+						<h5>Department of Environment and Natural Resources - CARAGA Region <h/5> 
+						<h3>DENR Regional ICT Caraga </h3> 
+                        <h6>&copy; Copyright 2022. All Rights Reserved </h6>
+                    </div>
+                </div>
+            </footer>
+			
+            <!-- End of Footer -->
