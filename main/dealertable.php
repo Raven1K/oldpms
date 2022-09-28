15095c6ee418a9760ad67c67aa63a699dd69ae1e
s0greedy <jaycee234@gmail.com>
2022-09-28 12:08:58 +0800

    test

--- main/dealertable.php
+++ main/dealertable.php
@@ -191,5 +191,9 @@
 	        .appendTo( '#example_wrapper .col-md-6:eq(0)' );
 	} );
      </script>
+	
+	 <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
