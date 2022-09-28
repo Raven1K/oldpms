2f89a130d2a95d600c62dbc4f5e06af05762eb5f
siege080 <112057191+siege080@users.noreply.github.com>
2022-09-28 10:52:55 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -26,7 +26,7 @@
           <a class="nav-link" href="register.php">REGISTER</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="register.php">FUS - CENRO</a>
+          <a class="nav-link" href="register.php">FUS-CENRO</a>
         </li>
       </ul>
     </div>
