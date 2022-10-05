c4cdf0942e3e82d3ab5715e249e3c34afd1958fd
s0greedy <jaycee234@gmail.com>
2022-10-05 14:31:24 +0800

    Update dealertable.php

--- main/dealertable.php
+++ main/dealertable.php
@@ -194,8 +194,7 @@
 	
 	 <!-- Bootstrap core JavaScript-->
 	<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="js/sb-admin-2.min.js"></script>
+
     
 </body>
 </html>
