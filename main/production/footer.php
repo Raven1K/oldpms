20f9d5cb9945b49a1ee3567d7f0cf46c4c7e014e
siege080 <112057191+siege080@users.noreply.github.com>
2022-10-07 07:52:45 +0800

    try

---
+++ main/production/footer.php
@@ -0,0 +1,12 @@
+<!-- footer content -->
+<footer class="footer-dark" style="background: #222222">
+<div class="copyright text-white my-auto border-top-0 d-sm-flex align-items-center justify-content-between mb-4">
+						<h6>Department of Environment and Natural Resources - CARAGA Region <h/6> 
+						<h5>DENR Regional ICT Caraga </h5> 
+                        <h6>&copy; Copyright 2022. All Rights Reserved </h6>
+                    </div>
+  <div class="clearfix"></div>
+</footer>
+<!-- /footer content -->
+</div>
+</div>
