38d27f5a93da445fd4f6a3ce8145941db91878a0
siege080 <112057191+siege080@users.noreply.github.com>
2022-11-04 09:33:47 +0800

    try

--- main/production/generate-pdf.php
+++ main/production/generate-pdf.php
@@ -1,4 +1,7 @@
 <?php
+session_start ();
+
+require_once "../../processphp/config.php";
 
 require __DIR__ . "/vendor/autoload.php";
 
@@ -22,6 +25,37 @@ $cabd = $_POST["cabd"];
 $mancu = $_POST["mancu"];
 $manbd = $_POST["manbd"];
 $bene = $_POST["bene"];
+
+$query = $connection->prepare("INSERT INTO c_endorsement(lumber_app_id, name, address, cons, planted, ptpoc, ptadd, falcu, falbd, macu, gecu, gebd, cacu, cabd, mancu, manbd, bene)
+VALUES (:lumber_app_id, :name,:address,:cons,:planted,:ptpoc, :ptadd, :falcu, :falbd,  :macu,  :gecu, :gebd, :cacu, :cabd, :mancu, :manbd, :bene)");
+$query->bindParam("lumber_app_id", $id, PDO::PARAM_STR);
+$query->bindParam("name", $name, PDO::PARAM_STR);
+$query->bindParam("address", $address, PDO::PARAM_STR);
+$query->bindParam("cons", $cons, PDO::PARAM_STR);
+$query->bindParam("planted", $planted, PDO::PARAM_STR);
+$query->bindParam("ptpoc", $ptpoc, PDO::PARAM_STR);
+$query->bindParam("ptadd", $ptadd, PDO::PARAM_STR);
+$query->bindParam("falcu", $falcu, PDO::PARAM_STR);
+$query->bindParam("falbd", $falbd, PDO::PARAM_STR);
+$query->bindParam("macu", $macu, PDO::PARAM_STR);
+$query->bindParam("gecu", $gecu, PDO::PARAM_STR);
+$query->bindParam("gebd", $gebd, PDO::PARAM_STR);
+$query->bindParam("cacu", $cacu, PDO::PARAM_STR);
+$query->bindParam("cabd", $cabd, PDO::PARAM_STR);
+$query->bindParam("mancu", $mancu, PDO::PARAM_STR);
+$query->bindParam("manbd", $manbd, PDO::PARAM_STR);
+$query->bindParam("bene", $bene, PDO::PARAM_STR);
+$result = $query->execute();
+
+
+$stat_uss = 'For Initial Chief RPS';
+$Flow_stats = '6.3';
+
+$sql = "UPDATE lumber_application SET Status = :Status, Flow_stat = :Flow_stat WHERE lumber_app_id = $id";
+$stmt = $connection->prepare($sql);
+$stmt->execute(array(
+':Status' => $stat_uss,
+':Flow_stat' => $Flow_stats,));
 
 
 /**
@@ -63,3 +97,4 @@ $dompdf->stream("endorsement.pdf", ["Attachment" => 0]);
  */
 $output = $dompdf->output();
 file_put_contents("file.pdf", $output);
+?>
